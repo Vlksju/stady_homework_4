6a314cd75fbc6ac9992dba2cf5ed2222b9e3a0f4
Vlksju <xenya.vol2017@yandex.ru>
2022-09-13 13:43:32 +0500

    Solved task 29

---
+++ task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа котрая выводит массив из 8 элементов, элементы массива вводятся в ручную");
+
+int [] arr = new int [8];
+
+for (int i = 0; i<arr.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива номер {i}");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+   Console.WriteLine (string.Join (", ", arr));
