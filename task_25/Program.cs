24830fd97f29d347263962885ebc8c7b7dfd1e38
Vlksju <xenya.vol2017@yandex.ru>
2022-09-13 13:34:13 +0500

    Solved task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа котрая принимает число A и B и возводит A в натуральную степень B");
+Console.WriteLine("Введите A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Результат = "); 
+int Sum = 1;
+
+while (B!= 0) {
+        Sum = Sum * A;
+        B--;
+    }
+
+Console.Write(Sum);
