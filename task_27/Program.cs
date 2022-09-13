d46ea2f87bfa44c4634a84e0125ba21b8a62b770
Vlksju <xenya.vol2017@yandex.ru>
2022-09-13 13:24:20 +0500

    Solved task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Программа которая принимает на вход число и выдает сумму цифр в нем");
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine ());
+
+int i = 0;
+
+while (N > 0) {
+        i = N % 10 + i;
+        N = N / 10;
+    }
+Console.WriteLine ("Сумма цифр в числе");
+Console.WriteLine (i);
+
+
